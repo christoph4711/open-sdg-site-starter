90e48cafe6ade845819740aea72ef5058c33944c
christoph4711 <christoph@biester.info>
2019-08-27 08:58:17 +0000

    Automated deployment to GitHub Pages: 0302d07543647db7fc939566c041f6b238957d2c

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1264,7 +1264,7 @@ var mapView = function () {
     $('#map').sdgMap({
       geoData: geoData,
       geoCodeRegEx: geoCodeRegEx,
-      mapOptions: {"minZoom":5,"maxZoom":10,"colorRange":"chroma.brewer.BuGn","noValueColor":null,"styleNormal":{"weight":1,"opacity":1,"color":null,"fillOpacity":0.7},"styleHighlighted":{"weight":1,"opacity":1,"color":null,"fillOpacity":0.7},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":null,"dashArray":55}},
+      mapOptions: {"minZoom":5,"maxZoom":10,"colorRange":["#F6E8EC","#EED3DB","#E5BFCA","#DDAAB9","#D495A8","#CC8197","#C46C86","#BB5775","#B34264","#AA2E53","#A21942"],"noValueColor":null,"styleNormal":{"weight":1,"opacity":1,"color":null,"fillOpacity":0.7},"styleHighlighted":{"weight":1,"opacity":1,"color":null,"fillOpacity":0.7},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":null,"dashArray":55}},
       mapLayers: [{"serviceUrl":"https://github.com/christoph4711/open-sdg-data-starter/blob/develop/NI_GJ.geojson","nameProperty":"urn:ogc:def:crs:OGC:1.3:CRS84","idProperty":"AGS","min_zoom":0,"max_zoom":20,"staticBorders":true}],
     });
   };
